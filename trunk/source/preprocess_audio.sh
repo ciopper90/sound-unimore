838ef045342e12ef2f46feb7dda6530e224313b1
nicola.bicocchi@gmail.com <nicola.bicocchi@gmail.com@bfa296e0-611a-5e6d-fda7-701dbf666cb2>
2012-05-22 17:27:31 +0000

    nb

--- trunk/source/preprocess_audio.sh
+++ trunk/source/preprocess_audio.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# NOTE:
+# - dobbiamo fare in modo che tutti i file abbiano 
+#   le stesse caratteristiche: (1 canale, 16bit, 11khz, 5sec di durata).
+#   il formato dei file ottenuti deve essere:
+#   classe_istanza_id
+#   classe = classe di appartenza
+#   istanza = registrazione di nicola sulla panda
+#   id = id del frammento di 5 secondi estratto da quella registrazione
+
 for file in ../audio/*; do
   sox "$file" -c 1 "$file.sox"
 done
