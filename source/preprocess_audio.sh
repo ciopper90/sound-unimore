840ba71a28e9bac4a5cc9467d07f254868282310
nicola.bicocchi@gmail.com <nicola.bicocchi@gmail.com@bfa296e0-611a-5e6d-fda7-701dbf666cb2>
2012-05-22 18:35:46 +0000

    nb: fix

--- source/preprocess_audio.sh
+++ source/preprocess_audio.sh
@@ -3,11 +3,8 @@
 # NOTE:
 # - dobbiamo fare in modo che tutti i file abbiano 
 #   le stesse caratteristiche: (1 canale, 16bit, 11khz, 5sec di durata).
-#   il formato dei file ottenuti deve essere:
-#   classe_istanza_id
-#   classe = classe di appartenza
-#   istanza = registrazione di nicola sulla panda
-#   id = id del frammento di 5 secondi estratto da quella registrazione
+#   il formato dei file ottenuti deve essere: classe_istanza.
+#   Se il file registrato è più lungo di 5 secondi, va spezzato.
 
 for file in ../audio/*; do
   sox "$file" -c 1 "$file.sox"
