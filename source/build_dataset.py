057c6abe2a35e797af6409c7c46465ee0ca03427
nicola.bicocchi@gmail.com <nicola.bicocchi@gmail.com@bfa296e0-611a-5e6d-fda7-701dbf666cb2>
2012-05-23 13:49:27 +0000

    nb: fix

--- source/build_dataset.py
+++ source/build_dataset.py
@@ -9,14 +9,16 @@ import scipy.io.wavfile as wavfile
 
 config = {
   'root_directory' : '../audio/',
+  'chunk_duration' : 2,
   'sample_duration' : 0.064,
-  'sample_number' : 5,
+  'sample_in_chunk' : 5,
   'output_format' : 'orange',
 }
 
 def features_extraction(sample):
     # per compilare questa lista puoi richiamare funzioni esterne
     # che ricevono in input un sample
+    print sample
     return [2, 2, 2]
     return
 
@@ -24,21 +26,38 @@ def samples_extraction(fname):
     samples = []    
     sample_rate, data = wavfile.read(fname)
     
+    file_data_points = len(data)
+    file_duration = file_data_points / sample_rate
     data_point_duration = 1.0 / sample_rate
+    
+    chunk_data_points = config['chunk_duration'] / data_point_duration
     sample_data_points = config['sample_duration'] / data_point_duration
     
-    n = 0
-    start_point = 0
-    step = len(data) / config['sample_number']
-    while n < config['sample_number']:
-        start_data_point = (len(data) / config['sample_number']) * n
-        end_data_point = start_data_point + sample_data_points
-        samples.append(data[start_data_point:end_data_point])
-        n += 1
+    n_chunks = int(file_duration / config['chunk_duration'])
+    n_sample = config['sample_in_chunk']
+
+    chunk = 0
+    while chunk < n_chunks:
+        sample = 0
+        while sample < n_sample:
+            start_data_point = (file_data_points / n_chunks) * chunk
+            start_data_point += (chunk_data_points / n_sample) * sample
+            end_data_point = start_data_point + sample_data_points
+            
+            samples.append({
+                'fname': fname,
+                'chunk_id': chunk,
+                'sample_id': sample,
+                'data': data[start_data_point:end_data_point],
+            })
+            
+            sample += 1
+        chunk += 1
     return samples
 
 def save(outdata):
-    pprint.pprint(outdata)
+    return
+    #pprint.pprint(outdata)
 
 def main():
     outdata = []
@@ -50,7 +69,7 @@ def main():
                 print '- found %s' % (fname,)
                 samples = samples_extraction(os.path.join(root, fname))
                 for sample in samples:
-                    features = features_extraction(sample)  
+                    features = features_extraction(sample)
                     features.insert(0, os.path.join(root, fname))
                     outdata.append(features)
     save(outdata)
